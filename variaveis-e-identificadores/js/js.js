9e22a795c8df95e02a075a440606958154564fef
Israel-Junqueira <israelribeiro313@gmail.com>
2022-11-03 09:20:30 -0300

    all

---
+++ variaveis-e-identificadores/js/js.js
@@ -0,0 +1,6 @@
+var nome;
+nome = "Pedro";
+var idade = 23;
+var pretensaoSalarial = 3277.22;
+var valeRefeicao = true;
+ console.log("Nome: " + nome + " Idade: " + idade + " Salário: " + pretensaoSalarial + " Vale Refeição: " + valeRefeicao);
